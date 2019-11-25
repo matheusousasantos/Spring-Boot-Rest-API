5e72aced5676e19654bf1dd84edbbf9fbbabcc57
matheus sousa <matheus.ads.study@gmail.com>
2019-11-25 14:42:45 -0300

    # 10 - Passando parâmetros para p serviço RESTfull

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -4,15 +4,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController/*Arquitetura REST*/
 @RequestMapping(value = "/usuario")
 public class IndexController {
 	
+	/*Serviço RESTfull*/
 	@GetMapping( value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity("Olá Usário REST Spring Boot", HttpStatus.OK);
+	public ResponseEntity init(@RequestParam(value = "nome", required = false) String nome) {
+		
+		System.out.println("Parâmetro sendo recebido " + nome);
+		return new ResponseEntity("Olá Usário REST Spring Boot, Seu nome é " + nome, HttpStatus.OK);
 	}
 	
 }
