a0b6aeb8e88135670dcee9962c52186cd0ff9934
matheus sousa <matheus.ads.study@gmail.com>
2019-12-10 20:56:09 -0300

    #  17  -  Criando a remoção com DELETE

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import curso.api.rest.model.Usuario;
 import curso.api.rest.repository.UsuarioRepository;
@@ -38,6 +40,14 @@ public class IndexController {
 		
 		Optional<Usuario> usuario = usuarioRepository.findById(id);	
 		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{id}", produces="application/text")
+	public String delete( @PathVariable("id") Long id ) {
+		
+		usuarioRepository.deleteById(id);
+		
+		return "OK!"; //Retorna apenas um OK
 	}
 	
 	@GetMapping( value="/", produces = "application/json")
