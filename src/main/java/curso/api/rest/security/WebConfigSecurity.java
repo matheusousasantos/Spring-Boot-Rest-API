0ea8d42fa8c70673c84abe1cb22dc7f4f3098a7a
matheus sousa <matheus.ads.study@gmail.com>
2020-01-06 21:11:04 -0300

    # 37 - Spring Security - Configurando o Spring Security

---
+++ src/main/java/curso/api/rest/security/WebConfigSecurity.java
@@ -0,0 +1,60 @@
+package curso.api.rest.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import curso.api.rest.service.ImplementacaoUserDatailsService;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private ImplementacaoUserDatailsService implementacaoUserDatailsService;
+	
+	/*Tudo que passar pelo sistema irá passar por essa validação( URL, permissões, etc.. )*/
+	/*Configura as solicitações de acesso por HTTP*/
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		/*Ativando a proteção contra usuários que não estão validados por Token*/
+		
+//        - Proteção conta ataque de usuários que não estão validados pelo nosso sistema
+		http.csrf().csrfTokenRepository(/*Tipo que ele vai trabalhar ->*/ CookieCsrfTokenRepository.withHttpOnlyFalse())
+		
+//      Ativando a retrição a URL -> Todo mundo pode acessar a parte inicial do Sistema.
+		.disable().authorizeRequests().antMatchers("/").permitAll()
+		.antMatchers("/index").permitAll()
+		
+//      URL de Logout - Redireciona após o user deslogar do sistema
+		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+		
+//      Mapeia URL de Logout e invalida o usuário
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		
+		
+//      ----------
+	
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		
+		
+		/*Service que irá consultar usuário no banco de dados*/
+		auth.userDetailsService(implementacaoUserDatailsService)
+		
+		/*Padrão de codificação de senha*/
+		.passwordEncoder(new BCryptPasswordEncoder());
+		
+	}
+
+}
